bd9cb8cf18ce516cac792175fa7a8e54dbc3fe4e
migangal <miguelalvfer@gmail.com>
2021-10-04 19:05:02 +0200

    first commit

---
+++ sql/sql_init.sql
@@ -0,0 +1,19 @@
+USE db1;
+
+CREATE TABLE directions(
+
+    direction VARCHAR(100) PRIMARY KEY;
+
+
+);
+
+CREATE TABLE USERS(
+
+    id VARCHAR(20) ;
+    nombre VARCHAR(40) NOT NULL;
+    edad int;
+    fecha_nac DATE;
+    fecha_ins DATE;
+
+);
+
