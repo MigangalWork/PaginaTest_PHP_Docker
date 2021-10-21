830054d184aecf8b951329654afce36d1d7b8df1
migangal <miguelalvfer@gmail.com>
2021-10-21 14:36:36 +0200

    sql fixed

--- sql/sql_init.sql
+++ sql/sql_init.sql
@@ -3,7 +3,7 @@ USE db1;
 
 CREATE TABLE USERS(
 
-    ,
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(40),
     edad int,
     fecha_nac DATE,
@@ -23,7 +23,7 @@ CREATE TABLE ENCUESTA(
     restricciones VARCHAR(100),
     fecha_inicio DATETIME,
     fecha_final DATETIME,
-    propietario INT,
+    propietario INT UNSIGNED,
 
     CONSTRAINT enc_users_fk FOREIGN KEY (propietario) REFERENCES USERS(id)
 
@@ -34,8 +34,8 @@ CREATE TABLE USERS_ENCUESTA(
     fecha_respuesta DATE,
     respondida BOOLEAN,
 
-    user INT PRIMARY KEY,
-    encuesta INT,
+    user INT UNSIGNED PRIMARY KEY,
+    encuesta INT UNSIGNED,
 
     CONSTRAINT ue_users_fk FOREIGN KEY (user) REFERENCES USERS(id),
     CONSTRAINT ue_encuesta_fk FOREIGN KEY (encuesta) REFERENCES ENCUESTA(id)
@@ -45,7 +45,7 @@ CREATE TABLE PREGUNTAS(
 
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     pregunta VARCHAR(100),
-    encuesta INT,
+    encuesta INT UNSIGNED,
 
     CONSTRAINT pre_encuesta_fk FOREIGN KEY (encuesta) REFERENCES ENCUESTA(id)
 );
@@ -54,7 +54,7 @@ CREATE TABLE RESPUESTAS(
 
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     respuesta VARCHAR(100),
-    pregunta INT,
+    pregunta INT UNSIGNED,
 
     CONSTRAINT res_preguntas_fk FOREIGN KEY (pregunta) REFERENCES PREGUNTAS(id)
 
